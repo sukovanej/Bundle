d7a692a6c599d5eadc2acc754420f13331a8c6a5
sukovanej <Milansuk@email.cz>
2015-01-03 11:47:46 +0100

    default theme didnt work with AllowComment value in Article database

--- themes/default/article_single.php
+++ themes/default/article_single.php
@@ -6,26 +6,33 @@
     </p></div>
     <div class="article_content"><?= $Article->Content ?></div>
     
-    
-    <!-- Přidat komentář -->
-    <div id="add_comment">
-        <?php if ((Bundle\User::IsLogged() || $Page->AllowUnregistredComments) && $Page->AllowComments): ?>
-            <form method="POST">
-                <table class="nostyle">
-                    <tr>
-                        <td><textarea name="bundle_comment_text"></textarea></td>
-                    </tr>
-                    <tr>
-                        <td><input type="submit" name="bundle_comment_submit" value="Přidat komentář" /></td>
-                    </tr>
-                </table>
-            </form>
-        <?php else: ?>
-            <em>Nejste přihlášený, pro přidávání komentářů <a href="./prihlaseni">se přihlašte</a>
-                nebo <a href="./registrace">registrujte</a>, prosím.</em>
-        <?php endif; ?>
-    </div>
-    <!-- Komentáře -->
-    
-    <?php $Article->Comments(); ?>
+    <?php if ($Article->AllowComments): ?>
+		<!-- Přidat komentář -->
+		<div id="add_comment">
+			<?php if ((Bundle\User::IsLogged() || $Page->AllowUnregistredComments) && $Page->AllowComments): ?>
+				<form method="POST">
+					<table class="nostyle">
+						<tr>
+							<td><textarea name="bundle_comment_text"></textarea></td>
+						</tr>
+						<tr>
+							<td><input type="submit" name="bundle_comment_submit" value="Přidat komentář" /></td>
+						</tr>
+					</table>
+				</form>
+			<?php else: ?>
+				<em>Nejste přihlášený, pro přidávání komentářů <a href="./prihlaseni">se přihlašte</a>
+					nebo <a href="./registrace">registrujte</a>, prosím.</em>
+			<?php endif; ?>
+		</div>
+		<!-- Komentáře -->
+		
+		
+		<?php $Article->Comments(); ?>
+		
+	<?php else: ?>
+		<div id="add_comment">
+			<em>Komentáře nejsou u tohoto článku povoleny.</em>
+		</div>
+	<?php endif; ?>
 </article>
