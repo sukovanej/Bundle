8497d26ed09f414aed8089b8d5201af8ab9eefd3
sukovanej <Milansuk@email.cz>
2015-01-06 10:54:18 +0100

    1.2.0.3 changes

--- func/bundle_Template.php
+++ func/bundle_Template.php
@@ -29,7 +29,7 @@ class Template {
 		}
 		
         $this->ThemeRoot = "./themes/" . $this->Theme;
-        $this->Bundle = "1.2.0.2"; // systém verzování = [větev].[verze].[sub-verze].[oprava]
+        $this->Bundle = "1.2.0.3"; // systém verzování = [větev].[verze].[sub-verze].[oprava]
         
         /*
          * Poslední oprava:
