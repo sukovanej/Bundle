192fd2fa2454f4231b3c3503e0d9e44dd048cfa7
sukovanej <Milansuk@email.cz>
2015-01-18 22:48:36 +0100

    bundle 1.2.0.4 changes

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	
 	define("_BD", "bundle");
 	
-	/** uncomment to enable PHP errors **/
+	/** uncomment to enable PHP errors **
 	ini_set('display_startup_errors',1);
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	/**/
+	**/
 	
 	if(filesize("config.ini") == 0)
 		header("location: ./install.php");
